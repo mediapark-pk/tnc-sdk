32562593e88f2bc061ca93567c34463ee72495c9
sarmad sohail <sarmad-sohail@hotmail.com>
2020-10-20 18:17:56 +0500

    update

--- src/Blocks/BlockFactory.php
+++ src/Blocks/BlockFactory.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 namespace TNC\Blocks;
 
 
+use Comely\Http\Exception\HttpRequestException;
+use Comely\Http\Exception\HttpResponseException;
+use Comely\Http\Exception\SSL_Exception;
+use TNC\Exception\TnxAPIException;
 use TNC\TncCoin;
 
 class BlockFactory
@@ -16,6 +20,14 @@ class BlockFactory
 
     public function getBlockByNumber(int $blockNumber) :Block
     {
-        $data =$this->tnc
+        $param = ["blockNum"=>$blockNumber];
+        try {
+            $data = $this->tnc->httpClient()->sendRequest("/api/getBlock",$param,[],"POST");
+            print_r($data);
+        } catch (HttpResponseException $e) {
+        } catch (SSL_Exception $e) {
+        } catch (HttpRequestException $e) {
+        } catch (TnxAPIException $e) {
+        }
     }
 }
