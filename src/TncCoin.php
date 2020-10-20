a989f128786ad982b0404813d3f73921732f9d1c
Almoeeni <you@example.com>
2020-10-20 16:51:01 +0500

    Tnc Coin Class

---
+++ src/TncCoin.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace TNC;
+
+
+use TNC\HttpClient;
+
+/**
+ * Class TncCoin
+ * @package TNC
+ */
+class TncCoin
+{
+    /** @var HttpClient */
+    private HttpClient $httpClient;
+
+    public function __construct(string $ip, ?int $port = NULL, ?string $username = "", ?string $password = "", bool $https = false)
+    {
+        $this->httpClient = new \TNC\HttpClient($ip, $port, $username, $password, $https);
+    }
+
+}
