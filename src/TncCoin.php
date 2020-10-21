7751765a50d19b0a16ee21ace625f9905ac02afa
Almoeeni <you@example.com>
2020-10-21 15:53:03 +0500

    tncCoin wif function created

--- src/TncCoin.php
+++ src/TncCoin.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 namespace TNC;
 use TNC\Exception\TncException;
+use TNC\Exception\TnxAPIException;
 use TNC\HttpClient;
 
 /**
@@ -87,5 +88,86 @@ class TncCoin
         throw new TncException("Server not working");
     }
 
+
+    /**
+     * @param string $username
+     * @param string $password
+     * @param string $role
+     * @return array
+     * @throws TnxAPIException
+     * @throws \Comely\Http\Exception\HttpRequestException
+     * @throws \Comely\Http\Exception\HttpResponseException
+     * @throws \Comely\Http\Exception\SSL_Exception
+     */
+    public function toWif(string $username, string $password, string $role) : array
+    {
+        $params = ['username' => $username, "password" => $password, "role" => $role];
+        $response =  $this->httpClient->sendRequest("toWif",$params,[],"POST");
+        if(!empty($response))
+        {
+            return $response;
+        }
+        throw new TnxAPIException("Server not working");
+    }
+
+    /**
+     * @param string $privatekey
+     * @return array
+     * @throws TnxAPIException
+     * @throws \Comely\Http\Exception\HttpRequestException
+     * @throws \Comely\Http\Exception\HttpResponseException
+     * @throws \Comely\Http\Exception\SSL_Exception
+     */
+    public function isWif(string $privatekey) : array
+    {
+        $params = ["wif" => $privatekey];
+       // return $this->httpClient->sendRequest("isWif",$params,[],"POST");
+        $response =  $this->httpClient->sendRequest("isWif",$params,[],"POST");
+        if(!empty($response))
+        {
+            return $response;
+        }
+        throw new TnxAPIException("Server not working");
+    }
+
+    /**
+     * @param string $privateKey
+     * @return array
+     * @throws TnxAPIException
+     * @throws \Comely\Http\Exception\HttpRequestException
+     * @throws \Comely\Http\Exception\HttpResponseException
+     * @throws \Comely\Http\Exception\SSL_Exception
+     */
+    public function wifToPublic(string $privateKey) : array
+    {
+        $params = ["private_key" => $privateKey];
+        $response =  $this->httpClient->sendRequest("wifToPublic",$params,[],"POST");
+        if(!empty($response))
+        {
+            return $response;
+        }
+        throw new TnxAPIException("Server not working");
+    }
+
+
+    /**
+     * @param string $privateKey
+     * @param string $publicKey
+     * @throws TnxAPIException
+     * @throws \Comely\Http\Exception\HttpRequestException
+     * @throws \Comely\Http\Exception\HttpResponseException
+     * @throws \Comely\Http\Exception\SSL_Exception
+     */
+    public function wifIsValid(string $privateKey , string $publicKey)
+    {
+        $params = ["private_key" => $privateKey , "public_key" => $publicKey];
+        $response = $this->httpClient->sendRequest("wifIsValid",$params,[],"POST");
+        if(!empty($response))
+        {
+            return $response;
+        }
+        throw new TnxAPIException("Server not working");
+
+    }
 
 }
